38932a41d60ff3d4b8f56f2400b1dafd7c2d695a
Nelson Ramirez <xhyskoax@gmail.com>
2023-07-22 12:50:03 -0400

    Se agrega lógica para traer usuarios con bootcamps y agregar usuarios a un bootcamp

--- app/controllers/bootcamp.controller.js
+++ app/controllers/bootcamp.controller.js
@@ -1,9 +1,60 @@
-export const createBootcamp = (req, res) => {
-    res.send("Creando bootcamp");
+import User from "../models/user.model.js";
+import Bootcamp  from "../models/bootcamp.model.js";
+
+export const createBootcamp = async (req, res) => {
+    let { title, cue, descripcion } = req.body;
+
+    let bootcamp = await Bootcamp.create({
+        title,
+        cue,
+        descripcion,
+    });
+
+    console.log("Se ha creado el bootcamp: ", bootcamp.toJSON());
+
+    res.status(201).json({
+        code: 201,
+        message: "bootcamp creado con éxito.",
+        bootcamp,
+    });
 };
 
-export const addUser = (req, res) => {
-    res.send("agregando usuario al bootcamp");
+export const addUser = async (req, res) => {
+
+    try {
+        let { bootcampId, userId } = req.body;
+
+        let bootcamp = await Bootcamp.findByPk(bootcampId);
+        let user = await User.findByPk(userId);
+
+        //SE AGREGA UN USUARIO AL BOOTCAMP.
+        await bootcamp.addUser(user);;
+
+        if (!bootcamp || !user) {
+            return res
+                .status(400)
+                .json({
+                    code: 400,
+                    message:
+                        "Usuario o bootcamp no existe, verifique los datos.",
+                });
+        }
+
+        //agregando usuario al bootcamp
+
+        console.log(
+            `Agregado el usuario id=${userId} al bootcamp con id=${bootcampId}`
+        );
+
+        res.status(201).json({
+            code: 201,
+            message: `Se ha vinculado correctamente el usuario: '${user.firstName}' al bootcamp: '${bootcamp.title}'`,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({code: 500, message: "Error al procesar la solicitud de agregar usuario al bootcamp."})
+    }
+
 };
 
 export const findById = (req, res) => {
--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Bootcamp from "../models/bootcamp.model.js";
 
 const createUser = async (req, res) => {
     let { firstName, lastName, email } = req.body;
@@ -13,8 +14,33 @@ const createUser = async (req, res) => {
     res.status(201).json({ code: 201, message: "usuario creado con éxito.", usuario });
 };
 
-const findUserById = (req, res) => {
-    res.send("traer usuario por id");
+const findUserById = async (req, res) => {
+    let id = req.params.id;
+
+    let usuario = await User.findByPk(id, {
+        include: [
+            {
+                model: Bootcamp,
+                as: "bootcamps",
+            },
+        ],
+    });
+
+    if (!usuario) {
+        return res.status(404).json({code: 404, message: "Usuario no encontrado."})
+    }
+
+    usuario = usuario.toJSON();
+
+    console.log("Usuario", usuario);
+
+    res.json({ code: 200, message: "Usuario encontrado.", usuario });
+    try {
+        
+    } catch (error) {
+        res.status(500).json({ code: 500, message: "Algo salió mal al filtrar el usuario por ID." })
+    }
+    
 };
 
 const findAll = (req, res) => {
